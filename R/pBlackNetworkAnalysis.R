0379e09aca056410ec5e9fce3063261b1ec0f67c
Robert Flight <r.flight79@gmail.com>
2014-06-06 09:51:03 -0400

    mapping aliases to STRING IDs

--- R/pBlackNetworkAnalysis.R
+++ R/pBlackNetworkAnalysis.R
@@ -18,3 +18,23 @@ communityAsList <- function(commObject, decreasing=TRUE){
 #' @source downloaded using KEGGREST from KEGG on June 4, 2014
 #' @details contains set of KGML xml data that P Blacks genes were annotated to as of June 4, 2014
 NULL
+
+#' map to STRING
+#' 
+#' @param query what you want to match on
+#' @param stringAliasFile the file with STRING aliases
+#' @export
+#' @return data.frame of translated queries
+map2STRING <- function(queryData, queryCol, stringAliasFile, removeUnmapped=TRUE){
+  aliasTable <- read.table(stringAliasFile, sep="\t", header=TRUE, stringsAsFactors=FALSE, quote="", fill=TRUE)
+  foundAlias <- aliasTable$alias %in% queryData[, queryCol]
+  aliasTable <- aliasTable[foundAlias, c("protein_id", "alias")]
+  
+  outData <- merge(queryData, aliasTable, by.x=queryCol, by.y="alias", all=TRUE)
+  
+  if (removeUnmapped){
+    naP <- is.na(outData$protein_id)
+    outData <- outData[!naP,]
+  }
+  return(outData)
+}
